44ddcb6d7760f90e9eecd0617d91b939c7d5b9a7
Tomas Ninno <126931839+Tomy0793@users.noreply.github.com>
2023-10-12 23:33:48 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -2,21 +2,26 @@ import './App.css';
 import ItemsListContainer from './components/ItemListContainer/ItemsListContainer';
 import NavBar from './components/NavBar/NavBar';
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import CartWidget from './components/CartWidget/CartWidget';
 import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer';
+import CartProvider from './components/Context/CartContext';
+import Cart from './components/Cart/Cart';
+import { Checkout } from './components/Checkout/Checkout';
 function App() {
   return (
     <div className="App">
 
       <BrowserRouter>
+      <CartProvider>
       <NavBar/>
       <Routes>
         <Route path= '/home' element ={<ItemsListContainer/>}/>
         <Route path= '/' element ={<ItemsListContainer/>}/>
         <Route path= '/category/:id' element ={<ItemsListContainer/>}/>
         <Route path= '/item/:id' element ={<ItemDetailContainer/>}/>
-        <Route path= '/cart' element ={<CartWidget/>}/>
+        <Route path= '/cart' element ={<Cart/>}/>
+        <Route path={"/checkout"} element={<Checkout/>} />
       </Routes>
+      </CartProvider>
       </BrowserRouter>
     </div> 
   );
