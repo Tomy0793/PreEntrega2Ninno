fc30cd3eb8f2fb1e34a88107f77b9888acd408f8
Tomas Ninno <126931839+Tomy0793@users.noreply.github.com>
2023-10-19 22:43:08 -0300

    Add files via upload

---
+++ src/Firebase/config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA-79CKlbOA2DdPxuK8BqY1MryN6ZqZWuE",
+  authDomain: "coderhouse-ecommerce-b11cd.firebaseapp.com",
+  projectId: "coderhouse-ecommerce-b11cd",
+  storageBucket: "coderhouse-ecommerce-b11cd.appspot.com",
+  messagingSenderId: "551458639786",
+  appId: "1:551458639786:web:5efea12b0129715459e5d3",
+  measurementId: "G-GD2RVZL42D"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
